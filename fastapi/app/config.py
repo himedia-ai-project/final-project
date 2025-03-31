bcce4c6663fbc1f273a5c6f96932d91d3b81386f
RudySeo <gun100491@gmail.com>
2025-03-31 21:36:00 +0900

    Feat: config 추가

--- fastapi/app/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-PGVECTOR_CONNECTION_STRING = os.getenv(
-    "PGVECTOR_CONNECTION_STRING",
-    "postgresql+psycopg2://myuser:mypassword@db:5432/mydb"
-)
