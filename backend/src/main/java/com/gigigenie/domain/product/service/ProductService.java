460f4a2474cfc2568f30af0bc487d64cee84191b
Heeyeon An <gmldus4613@naver.com>
2025-08-27 21:16:47 +0900

    remove(embedding): SpringBoot 내 임베딩 및 벡터DB 관련 코드 제거

--- backend/src/main/java/com/gigigenie/domain/product/service/ProductService.java
+++ backend/src/main/java/com/gigigenie/domain/product/service/ProductService.java
@@ -1,13 +1,9 @@
 package com.gigigenie.domain.product.service;
 
 import com.gigigenie.domain.product.dto.ProductResponse;
-
 import java.util.List;
 
 public interface ProductService {
 
     List<ProductResponse> list();
-
-    List<ProductResponse> findSimilarProducts(String query, Integer limit);
-
 }
