c3d8d26d041bd425296e0edce8f036c665b15eab
Heeyeon An <gmldus4613@naver.com>
2025-09-11 22:47:17 +0900

    refactor(product): Pdf 로직을 ProductController, ProductService로 통합

--- backend/src/main/java/com/gigigenie/domain/product/service/ProductService.java
+++ backend/src/main/java/com/gigigenie/domain/product/service/ProductService.java
@@ -2,8 +2,14 @@ package com.gigigenie.domain.product.service;
 
 import com.gigigenie.domain.product.dto.ProductResponse;
 import java.util.List;
+import java.util.Map;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface ProductService {
 
     List<ProductResponse> list();
+
+    Map<String, Object> processPdf(MultipartFile file, Integer categoryId, String name,
+        MultipartFile image, Authentication authentication);
 }
