165d31a3bda25c305ca41fbe69d05d1db161996e
RudySeo <gun100491@gmail.com>
2025-09-12 18:36:27 +0900

    refactor(ragWebClient): 클래스 이름 수정

--- backend/src/main/java/com/gigigenie/config/WebClientConfig.java
+++ backend/src/main/java/com/gigigenie/config/WebClientConfig.java
@@ -10,7 +10,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 @RequiredArgsConstructor
-public class RagClientConfig {
+public class WebClientConfig {
 
     @Value("${rag.base-url}")
     private String ragBaseUrl;
