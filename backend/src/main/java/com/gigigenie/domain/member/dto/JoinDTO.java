915938733396cb36bc82a2f4343ffd2c41022987
Heeyeon An <gmldus4613@naver.com>
2025-09-25 23:33:30 +0900

    fix(JoinDTO): 클래스명 변경(JoinRequestDTO -> JoinDTO) 및 이메일 유효성 검증을 @Email로 교체

--- backend/src/main/java/com/gigigenie/domain/member/dto/JoinDTO.java
+++ backend/src/main/java/com/gigigenie/domain/member/dto/JoinDTO.java
@@ -1,8 +1,14 @@
 package com.gigigenie.domain.member.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import lombok.*;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -10,13 +16,14 @@ import lombok.*;
 @Setter
 @Builder
 @ToString
-public class JoinRequestDTO {
+public class JoinDTO {
 
     @NotBlank(message = "이메일은 필수 입력 값입니다.")
-    @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$", message = "이메일 형식이 올바르지 않습니다.")
+    @Email(message = "이메일 형식이 올바르지 않습니다.")
     private String email;
 
     @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+    @Size(min = 4, max = 20, message = "비밀번호는 4자 이상 20자 이하로 입력해주세요.")
     private String password;
 
     @NotBlank(message = "유저이름은 필수 입력 값입니다.")
