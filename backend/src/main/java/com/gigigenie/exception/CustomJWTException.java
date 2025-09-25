bc0fe42b0578c4faacf5a00adbc4ecf4948128db
Heeyeon An <gmldus4613@naver.com>
2025-09-25 19:18:00 +0900

    remove(exception): OutOfStockException 클래스 제거

--- backend/src/main/java/com/gigigenie/exception/CustomJWTException.java
+++ backend/src/main/java/com/gigigenie/exception/CustomJWTException.java
@@ -1,13 +1,8 @@
 package com.gigigenie.exception;
 
 public class CustomJWTException extends RuntimeException {
+
     public CustomJWTException(String message) {
         super(message);
-    }
-
-    public static class OutOfStockException extends RuntimeException {
-        public OutOfStockException(String message) {
-            super(message);
-        }
     }
 }
